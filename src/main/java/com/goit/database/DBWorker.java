8dc586c5e7ec64ebaeaaf77308fb1c9abd453644
артур <mega9594@Rambler.ru>
2016-05-18 12:54:02 +0300

    Initial commit

---
+++ src/main/java/com/goit/database/DBWorker.java
@@ -0,0 +1,73 @@
+package com.goit.database;
+
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBWorker {
+    public  int counter = 0;
+    private final String URL = "jdbc:mysql://localhost:3306/calculationresult";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "3433905";
+    private final String INSERT_NEW = "INSERT INTO results VALUES(?,?,?)";
+    private final String DELETE_FROM_DB = "DELETE FROM results WHERE ID=?";
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+
+
+    public void addFormulaToDatabase(String userInput, String calculateFormulaResult) {
+
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            preparedStatement = connection.prepareStatement(INSERT_NEW);
+            preparedStatement.setInt(1, counter++);
+            preparedStatement.setString(2, userInput);
+            preparedStatement.setString(3, calculateFormulaResult);
+            preparedStatement.executeUpdate();
+            connection.commit();
+            connection.close();
+        }
+
+        catch (SQLException e){
+          e.printStackTrace();
+            //  System.out.println("Couldn't download class driver");
+        }
+
+    }
+
+    public void deleteDataFromDataBase(int id) {
+
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            preparedStatement = connection.prepareStatement(DELETE_FROM_DB);
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+            connection.commit();
+            connection.close();
+        }
+
+        catch (SQLException e) {
+            System.out.println("Couldn't download class driver");
+        }
+    }
+
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        DBWorker worker = new DBWorker();
+        worker.addFormulaToDatabase("d","y");
+    }
+
+
+}
---
+++ src/main/java/com/goit/logic/Main.java
@@ -0,0 +1,25 @@
+package com.goit.logic;
+
+
+import com.goit.logic.FormulaParser;
+import com.goit.logic.Function;
+
+public class Main {
+
+
+    public static String calculateUserFormula(String userInputFormula) {
+        String result;
+        double resultCalculateFormula;
+        try {
+            FormulaParser parser = new FormulaParser(userInputFormula);
+            Function function = parser.parse();
+            resultCalculateFormula = function.calculate();
+            result = Double.toString(resultCalculateFormula);
+        }
+        catch (Exception e) {
+           return "Некоректный ввод";
+        }
+
+        return result;
+    }
+}
